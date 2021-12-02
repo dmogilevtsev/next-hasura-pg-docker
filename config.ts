ed180f2f211e3ee9fd3b90b645b31efae8919ebb
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2021-12-02 12:57:00 +0300

    - remove files

--- config.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-export const client = new ApolloClient({
-	uri: 'https://promoted-man-37.hasura.app/v1/graphql',
-	cache: new InMemoryCache(),
-})
